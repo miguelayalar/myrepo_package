28543d669e34d92d397d51bafff8ae1b1352d7a7
Miguel Ayala <miguel.ayala.au@gmail.com>
2022-03-21 11:01:00 +1100

    Reder R script

--- rendered.R
+++ rendered.R
@@ -1,29 +1,28 @@
-#---
-#title: "Rendered R script"
-#author: "Miguel Ayala"
-#date: "21/03/2022"
-#output: html_document
-#keep_md: yes
-#---
+#' ---
+#' title: "Rendered R script"
+#' author: "Miguel Ayala"
+#' date: "21/03/2022"
+#' output: github_document
+#' ---
 
 #+ r setup, include=FALSE 
 knitr::opts_chunk$set(echo = TRUE)
 #
 
-## R Markdown
+##' R Markdown
 
-#This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+#' This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
 
-#When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
+#' When you click the Knit button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
 
 #+ r packages
 library(tidyverse)
 library(cowplot)
-#
+#'
 
-## Including Plots
+##' Including Plots
 
-#You can also embed plots, for example:
+#' You can also embed plots, for example:
 
 #+ r pressure, echo=FALSE
 econ <- economics 
@@ -35,6 +34,6 @@ econ %>%
   ggtitle('Unemployment in the USA',
           subtitle = 'number of unemployed in thousands') +
   cowplot::theme_cowplot()
-#
+#'
 
-#Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+#' Note that the echo = FALSE parameter was added to the code chunk to prevent printing of the R code that generated the plot.
---
+++ rendered.md
@@ -0,0 +1,37 @@
+Rendered R script
+================
+Miguel Ayala
+21/03/2022
+
+R Markdown This is an R Markdown document. Markdown is a simple
+formatting syntax for authoring HTML, PDF, and MS Word documents. For
+more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+When you click the Knit button a document will be generated that
+includes both content as well as the output of any embedded R code
+chunks within the document. You can embed an R code chunk like this:
+
+``` r
+library(tidyverse)
+```
+
+    ## ── Attaching packages ─────────────────────────────────────── tidyverse 1.3.1 ──
+
+    ## ✓ ggplot2 3.3.5     ✓ purrr   0.3.4
+    ## ✓ tibble  3.1.6     ✓ dplyr   1.0.8
+    ## ✓ tidyr   1.2.0     ✓ stringr 1.4.0
+    ## ✓ readr   2.1.2     ✓ forcats 0.5.1
+
+    ## ── Conflicts ────────────────────────────────────────── tidyverse_conflicts() ──
+    ## x dplyr::filter() masks stats::filter()
+    ## x dplyr::lag()    masks stats::lag()
+
+``` r
+library(cowplot)
+```
+
+Including Plots You can also embed plots, for example:
+
+![](rendered_files/figure-gfm/r%20pressure-1.png)<!-- -->
+
+Note that the echo = FALSE parameter was added to the code chunk to
+prevent printing of the R code that generated the plot.
